002d66b79710f7264cee1025cb2f3793714d9c04
jihami <w2027@e-mirim.hs.kr>
2022-01-04 21:50:14 +0900

    같은 숫자는 싫어

---
+++ ct.py
@@ -0,0 +1,9 @@
+def solution(table):
+    answer = 0
+    # for i in range(2):
+    #     answer = table[i][i]
+    # return answer
+    answer = table[0][0]
+    return table[0][0]
+
+solution([[1,0,0,1],[1,1,0,1],[0,1,0,1],[1,0,0,1]])
