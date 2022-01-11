e167fc5bf232612e35e27fedca5b8eee7d085da9
jihami <w2027@e-mirim.hs.kr>
2022-01-11 21:55:39 +0900

    직사각형 별찍기 - 자바

--- 직사각형 별찍기.py
+++ 직사각형 별찍기.py
@@ -1,3 +1,19 @@
 a, b = map(int, input().strip().split(' '))#a와 b의 값을 받아줌
 answer = ("*"*a+"\n")*b
 print(answer)
+#자바파일을 못만들어서 올려 놓는 자바
+'''
+import java.util.Scanner;
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i=0; i<b; i++){
+            for(int j=0; j<a; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}'''
